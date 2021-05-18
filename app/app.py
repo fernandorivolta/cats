7b4b2726447603396f25f3671edbdd6871478451
fernandorivolta <fe7rivolta@hotmail.com>
2021-05-18 19:58:09 -0300

    first commit

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+@app.route('/')
+def main():
+    return 'OK'
+
+app.run()
